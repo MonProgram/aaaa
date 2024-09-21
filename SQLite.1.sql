851c48dfa2ea012651a15800d70c6fea68fa102e
Jhonatan <138336588+MonProgram@users.noreply.github.com>
2024-09-20 21:48:22 -0300

    Add files via upload

---
+++ SQLite.1.sql
@@ -0,0 +1,12 @@
+INSERT INTO alunos VALUES (1, 'Adriano', 17, 'A');
+INSERT INTO alunos VALUES (2, 'Bernardo', 15, 'B');
+INSERT INTO alunos VALUES (3, 'Cristiane', 17, 'C');
+INSERT INTO alunos VALUES (4, 'Daniel', 17, 'A');
+INSERT INTO alunos VALUES (5, 'Evandro', 15, 'B');
+INSERT INTO alunos VALUES (6, 'Fabiana', 14, 'C');
+INSERT INTO alunos VALUES (7, 'Gabriely', 18, 'A');
+INSERT INTO alunos VALUES (8, 'Henrique', 17, 'B');
+INSERT INTO alunos VALUES (9, 'Kaio', 16, 'C');
+INSERT INTO alunos VALUES (10, 'Leandra', 17, 'C');
+---
+SELECT * FROM alunos WHERE idade = 17;
---
+++ SQLite.sql
@@ -0,0 +1,5 @@
+CREATE TABLE alunos (
+  id integer PRIMARY KEY,
+  nome text NOT NULL,
+  idade integer NOT NULL,
+  turma text NOT NULL);
