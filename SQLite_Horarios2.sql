c3b9735e33c96c0b6db73de85d2e28a8ecebe6ab
Jhonatan <138336588+MonProgram@users.noreply.github.com>
2024-10-04 08:56:07 -0300

    Add files via upload

---
+++ SQLite_Horarios2.sql
@@ -0,0 +1,7 @@
+INSERT INTO horario VALUES
+('07:30', 'Biologia - Michele Gehlen', 'Matemática - MA RCEL', 'Matemática - MA RCEL', 'Química - Aline e Fogaça', 'Programação Mobile - Luciano'),
+('08:20', 'Sociologia - Itamar', 'Língua Portuguesa - Marcia Leal', 'Língua Portuguesa - Marcia Leal', 'Língua Inglesa - Deise', 'Banco de Dados - GABRIEL'),
+('09:10', 'Projeto de Vida - Amauri', 'Programação Front-End - GABRIEL', 'Programação Front-End - GABRIEL', 'Química - Aline e Fogaça', 'Matemática - MA RCEL'),
+('10:20', 'Análise e Projeto de Sistemas - IAN', 'História - Michel Belo', 'Programação Front-End - GABRIEL', 'Geografia - Adélio', 'Biologia - Michele Gehlen'),
+('11:10', 'Análise e Projeto de Sistemas - IAN', 'Ciências da Computação - GABRIEL', 'Educação Financeira - Kellin', 'Programação Front-End - GABRIEL', 'Programação Mobile - Luciano'),
+('12:00', 'Sociologia - Itamar', 'Língua Portuguesa - Marcia Leal', 'Banco de Dados - GABRIEL', 'Geografia - Adélio', 'Ciências da Computação - GABRIEL');
